82bdfbf8b2a59838e9bdbd3632b411da92754c18
Mena Magdy <menamagdy475@gmail.com>
2026-05-21 03:02:59 +0300

    Remove unnecessary comment in Program.cs

--- Civil Registration System Platform/Program.cs
+++ Civil Registration System Platform/Program.cs
@@ -18,7 +18,7 @@ namespace Civil_Registration_System_Platform
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+//
             app.UseHttpsRedirection();
             app.UseRouting();
 
